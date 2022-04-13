f1cf0506130d3811ec51bb0169e31975dc6b3d98
rani <ranig1029@gmail.com>
2022-04-13 20:38:30 +0530

    EmpAttendance

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-       
+
        echo "Welcome to Employee Wage Computation"
+       EmpPresent=1;
+       EmpAbsent=2;
+       randomCheck=$((RANDOM%2+1));
+
+       if [ $randomCheck -eq $EmpPresent ];
+       then
+               echo "Emp is Present"
+       fi
+
+       if [ $randomCheck -eq $EmpAbsent ];
+       then
+               echo "Emp is Absent"
+       fi
+
