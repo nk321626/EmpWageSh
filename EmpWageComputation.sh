f58d2bfa6b68ad1213edecdb63ff815b8e495b50
rani <ranig1029@gmail.com>
2022-04-14 13:35:45 +0530

    EmpWage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
        echo "Welcome to Employee Wage Computation"
        EmpPresent=1;
@@ -10,8 +10,38 @@
                echo "Emp is Present"
        fi
 
-       if [ $randomCheck -eq $EmpAbsent ];
+       if [ $EmpAbsent -eq $randomCheck ];
        then
                echo "Emp is Absent"
        fi
 
+       EmpPresent=1;
+       randomCheck=$((RANDOM%2));
+
+       if [ $randomCheck -eq $EmpPresent ];
+       then
+
+       EmpRatePerHrs=20;
+       EmpHrs=8;
+       salary=$(($EmpRatePerHrs*$EmpHrs));
+else
+   salary=0;
+fi
+
+      EmpPartTime=1;
+      EmpFullTime=2;
+      EmpRatePerHrs=20;
+      randomCheck=$((RANDOM%3));
+
+      if [ $EmpFullTime -eq $randomCheck ];
+      then
+             EmpHrs=8;
+             echo "Emp is FullTime"
+
+      elif [ $EmpPartTime -eq $randomCheck ];
+      then
+             EmpHrs=4;
+             echo "Emp is PartTime"
+else
+   EmpHrs=0;
+fi
